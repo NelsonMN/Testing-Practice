1d2baafe5a1c45c2f69ac7954fc6b31b65d5a3b0
Nelson <nelson.mieszkalski@gmail.com>
2022-05-16 13:47:44 -0400

    Added calculator module with addition and subtraction tests

--- index.js
+++ index.js
@@ -14,4 +14,18 @@ function reverseString(str) {
     }
 }
 
-export { capitalize, reverseString}
+const calculator = (() => {
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b;
+
+    return {
+        add,
+        sub,
+        mul,
+        div,
+    }
+})();
+
+export { capitalize, reverseString, calculator }
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from './index'
+import { capitalize, reverseString, calculator } from './index'
 
 // capitalize():
 
@@ -15,8 +15,6 @@ test("One character string", () =>
 )
 
 
-
-
 // reverseString():
 
 test("Empty String", () => 
@@ -29,4 +27,40 @@ test("One letter", () =>
 
 test("More than one character", () =>
     expect(reverseString('hello')).toBe('olleh')
-    )
+)
+
+
+// calculator():
+
+// add() - addition is associative so we only need the three cases:
+test("Addition - two positive integers", () =>
+    expect(calculator.add(1, 2)).toBe(3)
+)
+
+test("Addition - one positive, one negative integer", () =>
+    expect(calculator.add(6, -4)).toBe(2)    
+)
+
+test("Addition - two negative integers", () => 
+    expect(calculator.add(-5, -2)).toBe(-7)
+)
+
+
+// sub():
+test("Subtraction - two positive integers", () => 
+    expect(calculator.sub(6, 5)).toBe(1)
+)
+
+test("Subtraction - first integer positive, second integer negative", () =>
+    expect(calculator.sub(5, -5)).toBe(10)
+)
+
+test("Subtraction - first integer negative, second integer positive", () =>
+    expect(calculator.sub(-2, 2)).toBe(-4)    
+)
+
+test("Subtraction - both negative integers", () =>
+    expect(calculator.sub(-2,-4)).toBe(2)
+)
+
+
